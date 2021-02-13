b4c695a825cb47f17405ce75bad4ca1019d42bb3
Kinnaird McQuade <kmcquade@salesforce.com>
2021-02-12 20:25:06 -0500

    Add RDS Snapshot list capability

--- terraform/all.tf
+++ terraform/all.tf
@@ -1,77 +1,85 @@
-module "acm_pca" {
-  source = "./acm-pca"
-}
-
-module "cloudwatch_resource_policy" {
-  source = "./cloudwatch-resource-policy"
-}
-
-module "ecr" {
-  source = "./ecr-repository"
-}
-
-module "efs" {
-  source = "./efs-file-system"
-}
-
-//module "elasticsearch_domain" {
-//  source = "./elasticsearch-domain"
+//module "acm_pca" {
+//  source = "./acm-pca"
+//}
+//
+//module "cloudwatch_resource_policy" {
+//  source = "./cloudwatch-resource-policy"
+//}
+//
+//module "ecr" {
+//  source = "./ecr-repository"
+//}
+//
+//module "efs" {
+//  source = "./efs-file-system"
+//}
+//
+////module "elasticsearch_domain" {
+////  source = "./elasticsearch-domain"
+////}
+//
+//module "glacier" {
+//  source = "./glacier-vault"
+//}
+//
+//module "iam_role" {
+//  source = "./iam-role"
+//}
+//
+//module "lambda_function" {
+//  source = "./lambda-function"
+//}
+//
+//module "lambda_layer" {
+//  source = "./lambda-layer"
+//}
+//
+//module "s3_bucket" {
+//  source = "./s3-bucket"
+//}
+//
+//
+//module "secrets_manager" {
+//  source = "./secrets-manager"
+//}
+//
+//module "ses_identity" {
+//  source      = "./ses-domain-identity"
+//}
+//
+//module "sns_topic" {
+//  source = "./sns-topic"
+//}
+//
+//module "sqs_queue" {
+//  source = "./sqs-queue"
 //}
 
-module "glacier" {
-  source = "./glacier-vault"
+module "rds_snapshot" {
+  source = "./rds-snapshot"
 }
-
-module "iam_role" {
-  source = "./iam-role"
-}
-
-module "lambda_function" {
-  source = "./lambda-function"
-}
-
-module "lambda_layer" {
-  source = "./lambda-layer"
-}
-
-module "s3_bucket" {
-  source = "./s3-bucket"
-}
-
-module "secrets_manager" {
-  source = "./secrets-manager"
-}
-
-module "ses_identity" {
-  source      = "./ses-domain-identity"
-}
-
-module "sns_topic" {
-  source = "./sns-topic"
-}
-
-module "sqs_queue" {
-  source = "./sqs-queue"
+output "names" {
+  value = module.rds_snapshot.snapshot_identifier
 }
 
 /*
 ElasticSearch Domain: ${module.elasticsearch_domain.name}
 */
 
-output "names" {
-  value = <<README
-ACM Private Certificate Authority (ACM PCA): ${module.acm_pca.arn}
-ECR Registry: ${module.ecr.name}
-EFS File System: ${module.efs.id}
-IAM Role: ${module.iam_role.name}
-Lambda Function: ${module.lambda_function.name}
-Lambda Layer: ${module.lambda_layer.name}
-S3 Bucket: ${module.s3_bucket.name}
-Secrets Manager: ${module.secrets_manager.name}
-SES Identity: ${module.ses_identity.name}
-SNS Topic: ${module.sns_topic.name}
-SQS Queue: ${module.sqs_queue.name}
-README
-}
+//output "names" {
+//  value = <<README
+//ACM Private Certificate Authority (ACM PCA): ${module.acm_pca.arn}
+//ECR Registry: ${module.ecr.name}
+//EFS File System: ${module.efs.id}
+//IAM Role: ${module.iam_role.name}
+//Lambda Function: ${module.lambda_function.name}
+//Lambda Layer: ${module.lambda_layer.name}
+//S3 Bucket: ${module.s3_bucket.name}
+//Secrets Manager: ${module.secrets_manager.name}
+//SES Identity: ${module.ses_identity.name}
+//SNS Topic: ${module.sns_topic.name}
+//SQS Queue: ${module.sqs_queue.name}
+//README
+//}
 
 // For the ACM Private Certificate Authority, you will need to access the AWS console after it is created. See the tutorial for more instructions.
--- terraform/rds-snapshot/db_snapshot.tf
+++ terraform/rds-snapshot/db_snapshot.tf
@@ -1,18 +1,69 @@
 resource "aws_db_instance" "bar" {
-  allocated_storage = 10
-  engine            = "MySQL"
-  engine_version    = "5.6.21"
-  instance_class    = "db.t2.micro"
-  name              = var.name
-  password          = "barbarbarbar"
-  username          = "foo"
-
-  maintenance_window      = "Fri:09:00-Fri:09:30"
-  backup_retention_period = 0
-  parameter_group_name    = "default.mysql5.6"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  multi_az = true
+  db_subnet_group_name = aws_db_subnet_group.default.name
 }
+//resource "aws_db_instance" "bar" {
+//  allocated_storage = 10
+//  engine            = "MySQL"
+//  engine_version    = "5.7"
+//  instance_class    = "db.t2.micro"
+//  name              = var.name
+//  password          = "barbarbarbar"
+//  username          = "foo"
+//
+//  maintenance_window      = "Fri:09:00-Fri:09:30"
+//  backup_retention_period = 0
+//  parameter_group_name    = "default.mysql5.6"
+//}
 
 resource "aws_db_snapshot" "test" {
   db_instance_identifier = aws_db_instance.bar.id
   db_snapshot_identifier = "${var.name}-db"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "subnet_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.1/28"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "subnet_1"
+  }
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.16/28"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "subnet_1"
+  }
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "test-resource-exposure-subnet"
+  subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+
+  tags = {
+    Name = "test-resource-exposure"
+  }
 }
